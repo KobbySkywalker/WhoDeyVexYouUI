cebfffeb0723d7e90941482bba147e8795388bb3
Hosny Savage <hosnysavage23@gmail.com>
2020-02-20 09:17:41 +0000

    Initial Commit

---
+++ Cool UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cool UI
+//
+//  Created by Hosny Ben Savage on 20/02/2020.
+//  Copyright © 2020 ITConsortium. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
